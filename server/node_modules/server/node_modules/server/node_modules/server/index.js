1b96e46c59cccf3b3ea9078d02806490cc255c82
sahil13082003 <sahilgolhar7@gmail.com>
2024-10-28 18:43:55 +0530

    mongo atlas added

--- server/node_modules/server/node_modules/server/node_modules/server/index.js
+++ server/node_modules/server/node_modules/server/node_modules/server/index.js
@@ -22,6 +22,6 @@ app.use('/api', timetableRoutes);
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
