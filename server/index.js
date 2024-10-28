3f5cfb07dd260c543258cae0cada15090175352f
sahil13082003 <sahilgolhar7@gmail.com>
2024-10-28 20:55:41 +0530

    .

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 // const mongoURI = 'mongodb://localhost:27017/Automatic_TimeTable_Generator';
   const mongoURI = process.env.MONGO_URL
 app.use(cors());
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
 
@@ -22,6 +22,6 @@ app.use('/api', timetableRoutes);
 
 
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5001; 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
